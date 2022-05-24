737e593079025384f6b92ea5e0961c8f36026ee2
Frank Palomino <frank.palomino1@gmail.com>
2022-05-24 23:19:42 +0200

    Probably last commit

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { Personaje } from "../interfaces/dbz.interface";
+
+@Injectable()
+export class DbzService {
+
+  private _personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 10000,
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 4000,
+    },
+  ]
+
+  get personajes(): Personaje[]{
+    return [... this._personajes];
+  }
+
+  agregarPersonaje( personaje: Personaje){
+    this._personajes.push(personaje);
+  }
+}
